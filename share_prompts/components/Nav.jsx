b3e36d96c0bf87c2c291354b20e6e16526a00226
fc55945 <marianavalente457@gmail.com>
2025-02-14 16:32:47 +0000

    import bug fix
    next-auth getProviders() was not being recognized and has now been fixed

--- share_prompts/components/Nav.jsx
+++ share_prompts/components/Nav.jsx
@@ -4,7 +4,8 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
-import { signIn, signOut, useSession, getProviders } from "next-auth";
+import { signIn, signOut, useSession} from "next-auth";
+import { getProviders } from "next-auth/react";
 
 
 
