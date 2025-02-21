648ef8b76908416ed4c7fc7f8764c4bdfd1e61fe
fc55945 <marianavalente457@gmail.com>
2025-02-21 16:44:03 +0000

    Fixes and User Model
    Fixed the broken authentication and created the User Model

--- share_prompts/app/api/auth/[...nextauth]/route.js
+++ share_prompts/app/api/auth/[...nextauth]/route.js
@@ -1,22 +1,33 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "utils/database";
+
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: '',
-            clientSecret: '',
-        })
-    ],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 
-    async session({ session }) {
-        
-    },
+  async session({ session, token }) {
+    
+  },
 
-    async signIn({ profile }) {
-        
-    },
-})
+  async signIn({ profile }) {
+      try {
+          await connectToDB();
+
+          //check if a user already exists
+
+
+          //if not, create a new user and add it to the db
+      } catch (error) {
+          console.log(error);
+          return false;
+    }   
+  },
+});
 
 export { handler as GET, handler as POST };
